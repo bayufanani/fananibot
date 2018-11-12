683fb4d1bd012911a79c3c5f6a58ac670e0302d5
Bayu Fanani <fanani707@gmail.com>
2018-11-12 12:56:53 +0700

    hellow

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-// in sublime
+const http = require('http');
+
+const port=process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+
+res.statusCode = 200;
+
+res.setHeader('Content-Type', 'text/html');
+
+res.end('<h1>Hello World</h1>');
+
+});
+
+server.listen(port,() => {
+
+console.log(`Server running at port `+port);
+
+});
+/* // in sublime
 var express = require('express');
 var port = process.env.PORT || 3000;
 
@@ -10,4 +29,4 @@ app.get('/', function (req, res) {
 
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
-});
+}); */
