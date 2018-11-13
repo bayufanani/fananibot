91d0f20717373e42a6a14ec8284b3b90989e29f7
Bayu Fanani <fanani707@gmail.com>
2018-11-13 11:27:11 +0700

    add cat command

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-const http = require('http');
+const TeleBot = require('telebot');
+const bot = new TeleBot('767888639:AAGOMOzFv3QJ8wxsjRgIbuMg7DCD1flsBjA');
 
-const port=process.env.PORT || 3000
+bot.on('text', (msg) => {
+  switch(msg.text.toLowerCase()) {
+    case 'kucing' : 
+      let promise;
+      let id = msg.chat.id;
 
-const server = http.createServer((req, res) => {
+      // Photo or gif?
 
-res.statusCode = 200;
+      promise = bot.sendPhoto(id, 'https://cataas.com/cat', {
+          fileName: 'kitty.jpg',
+          serverDownload: true
+      });
 
-res.setHeader('Content-Type', 'text/html');
+      // Send "uploading photo" action
+      bot.sendAction(id, 'upload_photo');
 
-res.end('<h1>Hello World</h1>');
-
+      return promise.catch(error => {
+        console.log('[error]', error);
+        // Send an error
+        bot.sendMessage(id, `😿 An error ${ error } occurred, try again.`);
+      });
+      break;
+    default : 
+      return msg.reply.text("gak paham! 😆 "+msg.text.toLowerCase());
+      break;
+  }
 });
 
-server.listen(port,() => {
-
-console.log(`Server running at port `+port);
-
-});
-/* // in sublime
-var express = require('express');
-var port = process.env.PORT || 3000;
-
-var app = express();
-
-app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
+bot.on(['/hello', '/start'], (msg) => {
+  msg.reply.text('hello');
 });
 
-app.listen(port, function () {
- console.log(`Example app listening on port !`);
-}); */
+bot.start();
