d84f73dd5765807672436f7795e7fa29f509fc6f
Bayu Fanani <fanani707@gmail.com>
2018-11-14 01:08:58 +0700

    move bot inside

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const TeleBot = require('telebot');
+const http = require('http')
+const port = 3000
 
-const bot = new TeleBot({
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  const bot = new TeleBot({
     token: process.env.API_TOKEN,
     webhook: {
-        // Self-signed certificate:
-        // key: './key.pem',
-        // cert: './cert.pem',
-        url: 'https://fananibot.herokuapp.com/',
-        host: '0.0.0.0',
-        port: 443
+      // Self-signed certificate:
+      // key: './key.pem',
+      // cert: './cert.pem',
+      url: 'https://fananibot.herokuapp.com/',
+      host: '0.0.0.0',
+      port: 443
     }
-});
+  });
 
-bot.on('text', msg => bot.sendMessage(msg.from.id, msg.text));
+  bot.on('text', msg => bot.sendMessage(msg.from.id, msg.text));
 
-bot.start();
+  bot.start();
+
+  console.log(`server is listening on ${port}`)
+})
