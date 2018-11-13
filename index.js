bfb6e6921b41d7e7c195051312ad39d1a070b9e3
Bayu Fanani <fanani707@gmail.com>
2018-11-14 01:25:51 +0700

    web server only

--- index.js
+++ index.js
@@ -14,21 +14,7 @@ server.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
-  const bot = new TeleBot({
-    token: process.env.API_TOKEN,
-    webhook: {
-      // Self-signed certificate:
-      // key: './key.pem',
-      // cert: './cert.pem',
-      url: 'https://fananibot.herokuapp.com',
-      host: '0.0.0.0',
-      port: 443
-    }
-  });
-
-  bot.on('text', msg => bot.sendMessage(msg.from.id, msg.text));
-
-  bot.start();
+  
 
   console.log(`server is listening on ${port}`)
 })
