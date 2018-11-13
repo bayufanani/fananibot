b3f28c1d95ea6445141036d0efa038b0e2885b52
Bayu Fanani <fanani707@gmail.com>
2018-11-13 12:21:39 +0700

    update express

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot('767888639:AAGOMOzFv3QJ8wxsjRgIbuMg7DCD1flsBjA');
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+app.get('/', function (req, res) {
+  res.send(JSON.stringify({ Hello: 'World'}));
+});
+app.listen(port, function () {
+  console.log(`Example app listening on port !`);
+});
