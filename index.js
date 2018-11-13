b3615296d71f32ee556a948834598ecdf579d44c
Bayu Fanani <fanani707@gmail.com>
2018-11-13 13:04:54 +0700

    pool

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TeleBot = require('telebot');
-// const bot = new TeleBot('767888639:AAGOMOzFv3QJ8wxsjRgIbuMg7DCD1flsBjA');
+const bot = new TeleBot('767888639:AAGOMOzFv3QJ8wxsjRgIbuMg7DCD1flsBjA');
 const http = require('http');
 const port=process.env.PORT || 3000
 
@@ -14,29 +14,6 @@ res.end('<h1>Hello World</h1>');
 });
 
 server.listen(port,() => {
-
-  const bot = new TeleBot({
-    token: '767888639:AAGOMOzFv3QJ8wxsjRgIbuMg7DCD1flsBjA', // Required. Telegram Bot API token.
-    /* polling: { // Optional. Use polling.
-        interval: 1000, // Optional. How often check updates (in ms).
-        timeout: 0, // Optional. Update polling timeout (0 - short polling).
-        limit: 100, // Optional. Limits the number of updates to be retrieved.
-        retryTimeout: 5000, // Optional. Reconnecting timeout (in ms).
-        proxy: 'http://username:password@yourproxy.com:8080' // Optional. An HTTP proxy to be used.
-    }, */
-    webhook: {
-        url: 'https://fananibot.heroku.com', // HTTPS url to send updates to.
-        host: '0.0.0.0', // Webhook server host.
-        port: 443, // Server port.
-        maxConnections: 40 // Optional. Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
-    },
-    allowedUpdates: [], // Optional. List the types of updates you want your bot to receive. Specify an empty list to receive all updates.
-    
-        // myPluginName: {
-        //   data: 'my custom value'
-        // }
-    
-});
 
   bot.on('text', (msg) => {
     switch(msg.text.toLowerCase()) {
