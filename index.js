50ab1f630901c04e8e6cae5afd1e06d451f8e96b
Bayu Fanani <fanani707@gmail.com>
2018-11-13 12:30:55 +0700

    reset

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const TeleBot = require('telebot');
-const bot = new TeleBot('767888639:AAGOMOzFv3QJ8wxsjRgIbuMg7DCD1flsBjA');
+const http = require('http');
+
+const port=process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+
+res.statusCode = 200;
+
+res.setHeader('Content-Type', 'text/html');
+
+res.end('<h1>Hello World</h1>');
+
+});
+
+server.listen(port,() => {
+
+console.log(`Server running at port `+port);
+
+});
+/* // in sublime
 var express = require('express');
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
+
 var app = express();
+
 app.get('/', function (req, res) {
-  res.send(JSON.stringify({ Hello: 'World'}));
+ res.send(JSON.stringify({ Hello: 'World'}));
 });
+
 app.listen(port, function () {
-  console.log(`Example app listening on port !`);
-});
+ console.log(`Example app listening on port !`);
+}); */
