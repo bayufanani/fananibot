496b3c611910307ca2b4b6d614e280c38e31962d
Bayu Fanani <fanani707@gmail.com>
2018-11-14 02:05:27 +0700

    add dummy command

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://fananibot.herokuapp.com';
 
 const bot = new Telegraf(API_TOKEN);
+
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
 bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
 bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
