386a5d0d8bc65dc2ec093e9577fdff60db54bbaa
Bayu Fanani <fanani707@gmail.com>
2018-11-13 12:33:16 +0700

    add bot

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const TeleBot = require('telebot');
+const bot = new TeleBot('767888639:AAGOMOzFv3QJ8wxsjRgIbuMg7DCD1flsBjA');
 const http = require('http');
-
 const port=process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
@@ -17,6 +18,40 @@ server.listen(port,() => {
 console.log(`Server running at port `+port);
 
 });
+
+bot.on('text', (msg) => {
+  switch(msg.text.toLowerCase()) {
+    case 'kucing' : 
+      let promise;
+      let id = msg.chat.id;
+
+      // Photo or gif?
+
+      promise = bot.sendPhoto(id, 'https://cataas.com/cat', {
+          fileName: 'kitty.jpg',
+          serverDownload: true
+      });
+
+      // Send "uploading photo" action
+      bot.sendAction(id, 'upload_photo');
+
+      return promise.catch(error => {
+        console.log('[error]', error);
+        // Send an error
+        bot.sendMessage(id, `😿 An error ${ error } occurred, try again.`);
+      });
+      break;
+    default : 
+      return msg.reply.text("gak paham! 😆 "+msg.text.toLowerCase());
+      break;
+  }
+});
+
+bot.on(['/hello', '/start'], (msg) => {
+  msg.reply.text('hello');
+});
+
+bot.start();
 /* // in sublime
 var express = require('express');
 var port = process.env.PORT || 3000;
